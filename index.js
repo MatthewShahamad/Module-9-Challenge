44bb6b2928e0f88c2a254fcc8ebd081cbc474d4e
MatthewShahamad <108310545+MatthewShahamad@users.noreply.github.com>
2022-09-09 01:09:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+[
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the app?',
+    },
+    {
+      type: 'input',
+      name: 'describe',
+      message: 'Describe what the app does',
+    },
+    {
+      type: 'input',
+      name: 'howTo',
+      message: 'How does this app work?',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'How do you install the app?',
+    },
+    {
+      type: 'input',
+      name: 'reportIssues',
+      message: 'How do users of the app report issues?',
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'How can users make contributions to the app?',
+    },
+  ]];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {'README.md', createReadMe, (err) =>
+      err ? console.log(err) : console.log('Successfully created README')
+    }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {  promptUser()
+    .then((answers) => writeFile('README.md', createReadMe(answers)))
+    .then(() => console.log('Successfully created README'))
+    .catch((err) => console.error(err));}
 
 // Function call to initialize app
 init();
